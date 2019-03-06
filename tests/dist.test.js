c62832b00e03c276b7c6c3b4dfe4b7dc3f3ac609
CD Cabrera <ccabrera@redhat.com>
2019-03-06 16:46:49 -0500

    chore(build): apply jest to dist output confirmation
    * npm scripts, distinction between jenkins, local run
    * unit test, snapshot for dist

---
+++ tests/dist.test.js
@@ -0,0 +1,17 @@
+const { execSync } = require('child_process');
+
+describe('Distribution', () => {
+  it('should match a specific file output', () => {
+    const output = execSync('find ./dist -type f -print0 | xargs -0');
+
+    const replacedGeneratedFilesMinsHash = output
+      .toString()
+      .replace(/\.\/dist/gi, '~./dist')
+      .replace(/\.([a-z0-9]+)\./gi, '*')
+      .split('~')
+      .sort()
+      .join('\n');
+
+    expect(replacedGeneratedFilesMinsHash).toMatchSnapshot('dist-output');
+  });
+});
