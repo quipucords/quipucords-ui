47dcbe09afab8059d65d1ee37095e397ccb0fce1
CD Cabrera <ccabrera@redhat.com>
2019-03-11 14:27:46 -0400

    fix(confirmationModal): upgrade pf react component
    * confirmationModal component switched from modal to messageDialog
    * unit tests expanded
    * styles updated, cleaned up

--- src/components/confirmationModal/__tests__/confirmationModal.test.js
+++ src/components/confirmationModal/__tests__/confirmationModal.test.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
-import { shallow } from 'enzyme';
-import ConfirmationModal from '../confirmationModal';
+import { mount, shallow } from 'enzyme';
+import { ConnectedConfirmationModal, ConfirmationModal } from '../confirmationModal';
 
-describe('ConfirmationModal Component', () => {
-  const generateEmptyStore = () => configureMockStore()({ confirmationModal: {} });
+describe('Confirmation Modal Component', () => {
+  const generateEmptyStore = (obj = {}) => configureMockStore()(obj);
 
-  it('should shallow render a basic component', () => {
-    const store = generateEmptyStore();
-    const props = { show: true };
-    const wrapper = shallow(<ConfirmationModal {...props} />, { context: { store } });
+  it('should render a connected component', () => {
+    const store = generateEmptyStore({
+      confirmationModal: {
+        show: true,
+        title: 'Confirm',
+        heading: 'test',
+        icon: null,
+        body: 'Test body',
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'Cancel'
+      }
+    });
+    const component = shallow(<ConnectedConfirmationModal />, { context: { store } });
 
-    expect(wrapper.dive()).toMatchSnapshot();
+    expect(component.dive()).toMatchSnapshot('connected');
+  });
+
+  it('should display a confirmation modal', () => {
+    const onCancel = jest.fn();
+    const props = {
+      show: true,
+      title: 'Confirm',
+      heading: 'test',
+      icon: null,
+      body: 'Test body',
+      confirmButtonText: 'Confirm',
+      cancelButtonText: 'Cancel',
+      onCancel
+    };
+
+    const component = mount(<ConfirmationModal {...props} />);
+
+    expect(component.render()).toMatchSnapshot('show');
+
+    component.find('button[className="btn btn-default"]').simulate('click');
+    expect(onCancel).toHaveBeenCalledTimes(1);
+  });
+
+  it('should NOT display a confirmation modal', () => {
+    const props = {
+      show: false
+    };
+
+    const component = mount(<ConfirmationModal {...props} />);
+
+    expect(component.render()).toMatchSnapshot('hidden');
   });
 });
