98b8da3b1f947cc5c5621a7856644a1a5a0fe51f
CD Cabrera <cabrera.code@gmail.com>
2019-04-16 12:02:54 -0400

    fix(app, auth, pageLayout, router):  issues/18 migrate prep pf4 (#47)
    * build, gitignore
    * router, routerConstants restructure, rename, unit tests
    * app, breakout aspects of auth & page layout, prep pf4
    * authentication, layer for confirming login auth
    * pageLayout, vertical nav and basic page layout
    * styling, naming alignment

---
+++ src/components/authentication/__tests__/authentication.test.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import { shallow, mount } from 'enzyme';
+import { ConnectedAuthentication, Authentication } from '../authentication';
+
+describe('Authentication Component', () => {
+  const generateEmptyStore = (obj = {}) => configureMockStore()(obj);
+
+  it('should render a connected component', () => {
+    const store = generateEmptyStore({
+      user: { session: { authorized: false, error: false, errorMessage: '', pending: false } }
+    });
+    const component = shallow(
+      <ConnectedAuthentication>
+        <span className="test">lorem</span>
+      </ConnectedAuthentication>,
+      { context: { store } }
+    );
+
+    expect(component).toMatchSnapshot('connected');
+  });
+
+  it('should render a non-connected component error', () => {
+    const props = {
+      session: {
+        authorized: false,
+        error: true,
+        errorMessage: 'Authentication credentials were not provided.',
+        pending: false
+      }
+    };
+    const component = mount(
+      <Authentication {...props}>
+        <span className="test">lorem</span>
+      </Authentication>
+    );
+
+    expect(component).toMatchSnapshot('non-connected error');
+  });
+
+  it('should render a non-connected component pending', () => {
+    const props = {
+      session: {
+        authorized: false,
+        error: false,
+        errorMessage: '',
+        pending: true
+      }
+    };
+    const component = shallow(
+      <Authentication {...props}>
+        <span className="test">lorem</span>
+      </Authentication>
+    );
+
+    expect(component).toMatchSnapshot('non-connected pending');
+  });
+
+  it('should render a non-connected component authorized', () => {
+    const props = {
+      session: {
+        authorized: true,
+        error: false,
+        errorMessage: '',
+        pending: false
+      }
+    };
+    const component = mount(
+      <Authentication {...props}>
+        <span className="test">lorem</span>
+      </Authentication>
+    );
+
+    expect(component).toMatchSnapshot('non-connected authorized');
+  });
+});
---
+++ src/components/pageLayout/pageLayout.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Icon, Masthead, MenuItem, VerticalNav } from 'patternfly-react';
+import { connectRouter, reduxActions, reduxTypes, store } from '../../redux';
+import helpers from '../../common/helpers';
+import { routes } from '../router/router';
+import titleImgBrand from '../../styles/images/title-brand.svg';
+import titleImg from '../../styles/images/title.svg';
+
+class PageLayout extends React.Component {
+  onAbout = () => {
+    store.dispatch({
+      type: reduxTypes.aboutModal.ABOUT_MODAL_SHOW
+    });
+  };
+
+  onLogout = () => {
+    const { logoutUser } = this.props;
+
+    logoutUser().finally(() => {
+      window.location = '/logout';
+    });
+  };
+
+  onNavigate = path => {
+    const { history } = this.props;
+
+    history.push(path);
+  };
+
+  onUnauthorized = () => {
+    window.location = '/logout';
+  };
+
+  renderIconBarActions() {
+    const { session } = this.props;
+
+    const title = (
+      <React.Fragment>
+        <Icon type="pf" name="user" /> {session && session.username}
+      </React.Fragment>
+    );
+
+    return (
+      <React.Fragment>
+        <Masthead.Dropdown id="app-help-dropdown" title={<span aria-hidden className="pficon pficon-help" />}>
+          <MenuItem eventKey="2" onClick={this.onAbout}>
+            About
+          </MenuItem>
+        </Masthead.Dropdown>
+        <Masthead.Dropdown id="app-user-dropdown" title={title}>
+          <MenuItem eventKey="3" onClick={this.onLogout}>
+            Logout
+          </MenuItem>
+        </Masthead.Dropdown>
+      </React.Fragment>
+    );
+  }
+
+  renderMenuActions() {
+    return [
+      <VerticalNav.Item key="about" className="collapsed-nav-item" title="About" onClick={this.onAbout} />,
+      <VerticalNav.Item key="logout" className="collapsed-nav-item" title="Logout" onClick={this.onLogout} />
+    ];
+  }
+
+  renderMenuItems() {
+    const { location, menu, isFullPage } = this.props;
+    const activeItem = menu.find(item => item.to.indexOf(location.pathname) > -1);
+
+    if (isFullPage) {
+      return null;
+    }
+
+    return menu.map(item => (
+      <VerticalNav.Item
+        key={item.to}
+        title={item.title}
+        iconClass={item.iconClass}
+        active={item === activeItem || (!activeItem && item.redirect)}
+        onClick={() => this.onNavigate(item.to)}
+      />
+    ));
+  }
+
+  render() {
+    const { brand, children, session } = this.props;
+
+    if (!session.authorized) {
+      return (
+        <div className="layout-pf layout-pf-fixed fadein">
+          <Masthead
+            titleImg={brand ? titleImgBrand : titleImg}
+            title="product discovery"
+            navToggle={false}
+            onTitleClick={this.onUnauthorized}
+          />
+          <div>{children}</div>
+        </div>
+      );
+    }
+
+    /**
+     * ToDo: Evaluate PF3-React VerticalNav vs PF4-React Page component. The component contributes to throwing a warning regarding unmounted components setting state.
+     * And forces the consumer to monitor more closely how state is managed. The warnings correlate to resize events. This warning can be squashed by using an
+     * `isMounted` state property to prevent render with a "null" return, or by avoiding the use of the onLayoutChange callback in the consuming component. This
+     * may be related to the PF4-React implementation around "onPageResize" where a check around the returned size helps squash a warning.
+     */
+    return (
+      <div className="layout-pf layout-pf-fixed fadein">
+        <VerticalNav persistentSecondary={false}>
+          <VerticalNav.Masthead>
+            <VerticalNav.Brand titleImg={brand ? titleImgBrand : titleImg} />
+            <VerticalNav.IconBar>{this.renderIconBarActions()}</VerticalNav.IconBar>
+          </VerticalNav.Masthead>
+          {this.renderMenuItems()}
+          {this.renderMenuActions()}
+        </VerticalNav>
+        <div className="container-pf-nav-pf-vertical">{children}</div>
+      </div>
+    );
+  }
+}
+
+PageLayout.propTypes = {
+  brand: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func
+  }),
+  isFullPage: PropTypes.bool,
+  location: PropTypes.shape({
+    pathname: PropTypes.string
+  }),
+  logoutUser: PropTypes.func,
+  menu: PropTypes.array,
+  session: PropTypes.shape({
+    authorized: PropTypes.bool,
+    username: PropTypes.string
+  })
+};
+
+PageLayout.defaultProps = {
+  brand: helpers.RH_BRAND,
+  history: {},
+  isFullPage: false,
+  location: {},
+  logoutUser: helpers.noop,
+  menu: routes,
+  session: {
+    authorized: false,
+    username: ''
+  }
+};
+
+const mapDispatchToProps = dispatch => ({
+  logoutUser: () => dispatch(reduxActions.user.logoutUser())
+});
+
+const mapStateToProps = state => ({
+  session: state.user.session
+});
+
+const ConnectedPageLayout = connectRouter(mapStateToProps, mapDispatchToProps)(PageLayout);
+
+export { ConnectedPageLayout as default, ConnectedPageLayout, PageLayout };
