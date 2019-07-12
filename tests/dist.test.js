306164d4fc9136498c974b58244ed71c69c6974a
CD Cabrera <ccabrera@redhat.com>
2019-07-12 19:47:18 -0400

    DEV integration testing snapshots
    * testing, integration testing snapshots
    * testing, localhost keyword, dotenv limitations check

--- tests/dist.test.js
+++ tests/dist.test.js
@@ -2,10 +2,9 @@ const { execSync } = require('child_process');
 
 describe('Distribution', () => {
   it('should match a specific file output', () => {
-    const output = execSync('find ./dist -type f -print0 | xargs -0');
+    const output = execSync('find ./dist -type f -print0 | xargs -0').toString();
 
     const replacedGeneratedFilesMinsHash = output
-      .toString()
       .replace(/\s+|\n+|\r+/g, '')
       .replace(/\.\/dist/gi, '~./dist')
       .replace(/\.([a-z0-9]+)\./gi, '*')
@@ -13,5 +12,11 @@ describe('Distribution', () => {
       .sort();
 
     expect(replacedGeneratedFilesMinsHash).toMatchSnapshot('dist-output');
+  });
+
+  it('should not contain references to localhost', () => {
+    const output = execSync(`grep -roi "localhost:" ./dist/client/static | wc -l`).toString();
+
+    expect(Number.parseInt(output.trim(), 10)).toBe(0);
   });
 });
