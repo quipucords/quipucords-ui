f8334d4ea4f44abc747dab511af9190d07ac72b6
CD Cabrera <ccabrera@redhat.com>
2019-12-10 11:22:15 -0500

    fix(build): issues/73 correct Django token reference
    * dotenv, build with the production csrftoken
    * tests, added dist check for Ajax environment params

--- tests/dist.test.js
+++ tests/dist.test.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const { execSync } = require('child_process');
 
 describe('Distribution', () => {
+  const loadFile = file => (fs.existsSync(file) && fs.readFileSync(file, { encoding: 'utf-8' })) || '';
   const trimSortDistList = str =>
     str
       .replace(/\s+|\n+|\r+/g, '')
@@ -19,5 +21,14 @@ describe('Distribution', () => {
     const output = execSync(`grep -roi "localhost:" ./dist/client/static | wc -l`).toString();
 
     expect(Number.parseInt(output.trim(), 10)).toBe(0);
+  });
+
+  it('should have specific ajax environment parameters set', () => {
+    const envProductionLocalFile = loadFile('./.env.production');
+
+    const ajaxToken = envProductionLocalFile.match(/\bREACT_APP_AUTH_TOKEN=(.*)\n/i)[1];
+    const tokenWithinGui = execSync(`grep -rl "${ajaxToken}" ./dist/client/static/js`).toString();
+
+    expect({ ajaxToken, files: trimSortDistList(tokenWithinGui) }).toMatchSnapshot('ajax parameters');
   });
 });
