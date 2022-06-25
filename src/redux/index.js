d2c2868b251fe5bf9f7830d3710608fc1a238110
CD Cabrera <ccabrera@redhat.com>
2022-06-25 01:54:47 -0400

    refactor(aboutModal): discovery-148 pf4 conversion (#87)
    * aboutModal, pf4 component conversion
    * locales, about modal strings

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { withTranslation } from 'react-i18next';
 import { store } from './store';
 import { reduxActions } from './actions';
 import { reduxHelpers } from './common';
@@ -8,13 +7,6 @@ import { storeHooks } from './hooks';
 import { reduxReducers } from './reducers';
 import { reduxSelectors } from './selectors';
 import { reduxTypes } from './constants';
-import { helpers } from '../common/helpers';
-
-const connectTranslate = (mapStateToProps, mapDispatchToProps) => component =>
-  connect(mapStateToProps, mapDispatchToProps)((!helpers.TEST_MODE && withTranslation()(component)) || component);
-
-const connectRouterTranslate = (mapStateToProps, mapDispatchToProps) => component =>
-  withRouter(connectTranslate(mapStateToProps, mapDispatchToProps)(component));
 
 const connectRouter = (mapStateToProps, mapDispatchToProps) => component =>
   withRouter(connect(mapStateToProps, mapDispatchToProps)(component));
@@ -22,8 +14,6 @@ const connectRouter = (mapStateToProps, mapDispatchToProps) => component =>
 export {
   connect,
   connectRouter,
-  connectRouterTranslate,
-  connectTranslate,
   reduxActions,
   reduxHelpers,
   reduxReducers,
