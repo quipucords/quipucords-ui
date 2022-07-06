47975db51a869be3e44cdf23e4e438dfaf860345
CD Cabrera <ccabrera@redhat.com>
2022-07-06 15:07:55 -0400

    fix(confirmationModal): expand behavior, allow children (#113)

--- src/components/confirmationModal/confirmationModal.js
+++ src/components/confirmationModal/confirmationModal.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Alert, AlertVariant, Button, ButtonVariant, Title } from '@patternfly/react-core';
+import classNames from 'classnames';
+import { Alert, AlertVariant as ConfirmationVariant, Button, ButtonVariant, Title } from '@patternfly/react-core';
 import { Modal } from '../modal/modal';
 import { connect, store, reduxTypes } from '../../redux';
 import { translate } from '../i18n/i18n';
 
+/**
+ * Display a confirmation modal with actions.
+ *
+ * @param {object} props
+ * @param {React.ReactNode} props.body
+ * @param {React.ReactNode} props.children
+ * @param {string} props.cancelButtonText
+ * @param {string} props.confirmButtonText
+ * @param {React.ReactNode} props.heading
+ * @param {string} props.icon
+ * @param {boolean} props.isClose
+ * @param {boolean} props.isContentOnly
+ * @param {boolean} props.isActions
+ * @param {Function} props.onCancel
+ * @param {Function} props.onConfirm
+ * @param {boolean} props.show
+ * @param {Function} props.t
+ * @param {string} props.title
+ * @returns {React.ReactNode}
+ */
 const ConfirmationModal = ({
-  show,
-  title,
-  heading,
   body,
-  icon,
-  confirmButtonText,
+  children,
   cancelButtonText,
-  onConfirm,
+  confirmButtonText,
+  heading,
+  icon,
+  isClose,
+  isContentOnly,
+  isActions,
   onCancel,
-  t
+  onConfirm,
+  show,
+  t,
+  title
 }) => {
   const cancel = () => {
     if (onCancel) {
@@ -29,6 +54,10 @@ const ConfirmationModal = ({
 
   const setActions = () => {
     const actions = [];
+
+    if (!isActions) {
+      return actions;
+    }
 
     if (onConfirm) {
       actions.push(
@@ -47,44 +76,77 @@ const ConfirmationModal = ({
     return actions;
   };
 
+  const updatedChildren =
+    body || heading ? (
+      <Alert isInline isPlain variant={icon || undefined} title={heading}>
+        <p>{body}</p>
+      </Alert>
+    ) : (
+      children
+    );
+
   return (
     <Modal
-      className="quipucords-modal__confirmation"
+      className={classNames('quipucords-modal__confirmation', {
+        'quipucords-modal__content-only': isContentOnly === true
+      })}
       isOpen={show}
       backdrop={false}
-      showClose
+      showClose={isClose}
       onClose={cancel}
       actions={setActions()}
       header={
-        <Title headingLevel="h4">{title || t('form-dialog.label', { context: ['submit', 'confirmation'] })}</Title>
+        title && (
+          <Title headingLevel="h4">{title || t('form-dialog.label', { context: ['submit', 'confirmation'] })}</Title>
+        )
       }
       disableFocusTrap
     >
-      <Alert isInline isPlain variant={icon || undefined} title={heading}>
-        {body && <p>{body}</p>}
-      </Alert>
+      {updatedChildren || ''}
     </Modal>
   );
 };
 
+/**
+ * Prop types
+ *
+ * @type {{isClose: boolean, isActions: boolean, heading: React.ReactNode, icon: string, body: React.ReactNode, title: string,
+ *     show: boolean, t: Function, children: React.ReactNode, onCancel: Function, onConfirm: Function, isContentOnly: boolean,
+ *     cancelButtonText: string, confirmButtonText: string}}
+ */
 ConfirmationModal.propTypes = {
-  show: PropTypes.bool.isRequired,
-  title: PropTypes.string,
-  heading: PropTypes.node,
-  icon: PropTypes.node,
   body: PropTypes.node,
-  confirmButtonText: PropTypes.string,
   cancelButtonText: PropTypes.string,
-  onConfirm: PropTypes.func,
+  children: PropTypes.node,
+  confirmButtonText: PropTypes.string,
+  heading: PropTypes.node,
+  icon: PropTypes.oneOf([...Object.values(ConfirmationVariant)]),
+  isActions: PropTypes.bool,
+  isClose: PropTypes.bool,
+  isContentOnly: PropTypes.bool,
+  show: PropTypes.bool.isRequired,
   onCancel: PropTypes.func,
-  t: PropTypes.func
+  onConfirm: PropTypes.func,
+  t: PropTypes.func,
+  title: PropTypes.string
 };
 
+/**
+ * Default props.
+ *
+ * @type {{isClose: boolean, isActions: boolean, heading: null, icon: ConfirmationVariant.warning, title: null, body: null,
+ *     t: translate, children: null, onCancel: null, onConfirm: null, isContentOnly: boolean, confirmButtonText: null,
+ *     cancelButtonText: null}}
+ */
 ConfirmationModal.defaultProps = {
+  children: null,
   title: null,
   heading: null,
   body: null,
-  icon: AlertVariant.warning,
+  icon: ConfirmationVariant.warning,
+  isActions: true,
+  isClose: true,
+  isContentOnly: false,
   confirmButtonText: null,
   cancelButtonText: null,
   onConfirm: null,
@@ -96,4 +158,4 @@ const mapStateToProps = state => ({ ...state.confirmationModal });
 
 const ConnectedConfirmationModal = connect(mapStateToProps)(ConfirmationModal);
 
-export { ConnectedConfirmationModal as default, ConnectedConfirmationModal, ConfirmationModal };
+export { ConnectedConfirmationModal as default, ConnectedConfirmationModal, ConfirmationModal, ConfirmationVariant };
