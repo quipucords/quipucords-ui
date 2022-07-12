dec89b85bb2278222729a7fe42d1cf1d139172ba
CD Cabrera <ccabrera@redhat.com>
2022-07-12 17:22:33 -0400

    fix(testing): expand toastNotificationsList snapshots (#115)

--- src/components/toastNotificationsList/__tests__/toastNotificationsList.test.js
+++ src/components/toastNotificationsList/__tests__/toastNotificationsList.test.js
@@ -2,20 +2,66 @@ import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
-import ToastNotificationsList from '../toastNotificationsList';
+import { AlertVariant } from '@patternfly/react-core';
+import { ConnectedToastNotificationsList, ToastNotificationsList } from '../toastNotificationsList';
 
 describe('ToastNotificationsList Component', () => {
   const generateEmptyStore = () => configureMockStore()({ toastNotifications: {} });
 
   it('should shallow render a basic component', () => {
     const store = generateEmptyStore();
-    const props = { show: true };
-    const wrapper = shallow(
+    const props = {};
+    const component = shallow(
       <Provider store={store}>
-        <ToastNotificationsList {...props} />
+        <ConnectedToastNotificationsList {...props} />
       </Provider>
     );
 
-    expect(wrapper.find(ToastNotificationsList)).toMatchSnapshot();
+    expect(component.find(ConnectedToastNotificationsList)).toMatchSnapshot('basic');
+  });
+
+  it('should handle toast variations', () => {
+    const props = {
+      toasts: [
+        {
+          alertType: undefined,
+          header: undefined,
+          message: 'Lorem ipsum',
+          removed: undefined,
+          paused: undefined
+        },
+        {
+          alertType: AlertVariant.success,
+          header: 'Dolor sit',
+          message: undefined,
+          removed: undefined,
+          paused: undefined
+        },
+        {
+          alertType: AlertVariant.danger,
+          header: 'Dolor sit',
+          message: 'Lorem ipsum',
+          removed: undefined,
+          paused: undefined
+        },
+        {
+          alertType: AlertVariant.info,
+          header: 'REMOVED, Dolor sit',
+          message: undefined,
+          removed: true,
+          paused: undefined
+        },
+        {
+          alertType: AlertVariant.info,
+          header: 'PAUSED, Dolor sit',
+          message: undefined,
+          removed: undefined,
+          paused: true
+        }
+      ]
+    };
+    const component = shallow(<ToastNotificationsList {...props} />);
+
+    expect(component).toMatchSnapshot('variations');
   });
 });
