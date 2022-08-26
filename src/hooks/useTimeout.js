a52d9ef34faf0410e6735f203e8b3241d557fe28
CD Cabrera <ccabrera@redhat.com>
2022-08-26 11:52:28 -0400

    chore(useTimeout): hook annotation (#152)

--- src/hooks/useTimeout.js
+++ src/hooks/useTimeout.js
@@ -2,11 +2,11 @@ import { useEffect, useRef, useState } from 'react';
 import { helpers } from '../common';
 
 /**
- * setTimeout hook
+ * window.setTimeout hook polling hook with multiple cancel alternatives
  *
- * @param {Function} callback
+ * @param {function(): boolean} callback Callback can return a boolean, with false cancelling the timeout.
  * @param {number} pollInterval
- * @returns {Function}
+ * @returns {{cancel: function(): void, update: number|undefined}} A cancel function is returned, calling it cancels the setTimeout. The returned "update" value is a time increment, always adding up, and purposefully causing a hook update.
  */
 const useTimeout = (callback, pollInterval = 0) => {
   const timer = useRef();
