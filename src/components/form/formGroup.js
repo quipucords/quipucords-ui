7011a0e7e5458bf47df2ac3156f834e779b8da85
CD Cabrera <ccabrera@redhat.com>
2022-09-13 23:12:30 -0400

    fix(formGroup): discovery-153 use validatedOptions (#161)

--- src/components/form/formGroup.js
+++ src/components/form/formGroup.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormGroup as PfFormGroup } from '@patternfly/react-core';
+import { FormGroup as PfFormGroup, ValidatedOptions } from '@patternfly/react-core';
 import helpers from '../../common/helpers';
 
-const FormGroup = ({ children, error, errorMessage, helperText, id, label, ...props }) => {
+/**
+ * A wrapper for Patternfly FormGroup.
+ *
+ * @param {object} props
+ * @param {React.ReactNode} props.children
+ * @param {string|boolean} props.error
+ * @param {React.ReactNode|boolean} props.errorMessage
+ * @param {React.ReactNode} props.helperText
+ * @param {string} props.id
+ * @param {object} props.props
+ * @returns {React.ReactNode}
+ */
+const FormGroup = ({ children, error, errorMessage, helperText, id, ...props }) => {
   const setId = id || helpers.generateId();
 
   return (
     <PfFormGroup
       id={setId}
-      validated={error ? 'error' : null}
+      validated={error ? ValidatedOptions.error : ValidatedOptions.default}
       // FixMe: Applies consistent behavior to helperText regardless of it being an element or string
       helperText={helperText && <div className="pf-c-form__helper-text">{helperText}</div>}
       helperTextInvalid={
@@ -18,7 +30,6 @@ const FormGroup = ({ children, error, errorMessage, helperText, id, label, ...pr
         (errorMessage && 'Error') ||
         ''
       }
-      label={label}
       {...props}
     >
       {children}
@@ -26,21 +37,30 @@ const FormGroup = ({ children, error, errorMessage, helperText, id, label, ...pr
   );
 };
 
+/**
+ * Prop types
+ *
+ * @type {{children: React.ReactNode, errorMessage: React.ReactNode|boolean, id: string, error: string|boolean,
+ *     helperText: React.ReactNode}}
+ */
 FormGroup.propTypes = {
   children: PropTypes.node.isRequired,
   error: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   errorMessage: PropTypes.oneOfType([PropTypes.node, PropTypes.bool]),
   helperText: PropTypes.node,
-  id: PropTypes.string,
-  label: PropTypes.node
+  id: PropTypes.string
 };
 
+/**
+ * Default props
+ *
+ * @type {{errorMessage: null, id: null, error: null, helperText: null}}
+ */
 FormGroup.defaultProps = {
   error: null,
   errorMessage: null,
   helperText: null,
-  id: null,
-  label: null
+  id: null
 };
 
 export { FormGroup as default, FormGroup };
