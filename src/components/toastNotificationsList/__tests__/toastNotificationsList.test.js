4dc33310450c9ad1867873ed98a3caad776c3d0c
CD Cabrera <ccabrera@redhat.com>
2022-10-10 12:01:50 -0400

    perf(toastNotificationsList): toasts to hooks (#180)

--- src/components/toastNotificationsList/__tests__/toastNotificationsList.test.js
+++ src/components/toastNotificationsList/__tests__/toastNotificationsList.test.js
@@ -1,67 +1,93 @@
 import React from 'react';
-import configureMockStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
-import { shallow } from 'enzyme';
 import { AlertVariant } from '@patternfly/react-core';
-import { ConnectedToastNotificationsList, ToastNotificationsList } from '../toastNotificationsList';
+import { ToastNotificationsList, useOnDismiss, useOnHover, useOnLeave } from '../toastNotificationsList';
+import { store } from '../../../redux';
 
 describe('ToastNotificationsList Component', () => {
-  const generateEmptyStore = () => configureMockStore()({ toastNotifications: {} });
+  let mockDispatch;
 
-  it('should shallow render a basic component', () => {
-    const store = generateEmptyStore();
-    const props = {};
-    const component = shallow(
-      <Provider store={store}>
-        <ConnectedToastNotificationsList {...props} />
-      </Provider>
-    );
-
-    expect(component.find(ConnectedToastNotificationsList)).toMatchSnapshot('basic');
+  beforeEach(() => {
+    mockDispatch = jest.spyOn(store, 'dispatch').mockImplementation((type, data) => ({ type, data }));
   });
 
-  it('should handle toast variations', () => {
-    const props = {
-      toasts: [
-        {
-          alertType: undefined,
-          header: undefined,
-          message: 'Lorem ipsum',
-          removed: undefined,
-          paused: undefined
-        },
-        {
-          alertType: AlertVariant.success,
-          header: 'Dolor sit',
-          message: undefined,
-          removed: undefined,
-          paused: undefined
-        },
-        {
-          alertType: AlertVariant.danger,
-          header: 'Dolor sit',
-          message: 'Lorem ipsum',
-          removed: undefined,
-          paused: undefined
-        },
-        {
-          alertType: AlertVariant.info,
-          header: 'REMOVED, Dolor sit',
-          message: undefined,
-          removed: true,
-          paused: undefined
-        },
-        {
-          alertType: AlertVariant.info,
-          header: 'PAUSED, Dolor sit',
-          message: undefined,
-          removed: undefined,
-          paused: true
-        }
-      ]
-    };
-    const component = shallow(<ToastNotificationsList {...props} />);
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
+  it('should render a basic component', async () => {
+    const props = {};
+    const component = await shallowHookComponent(<ToastNotificationsList {...props} />);
+
+    expect(component).toMatchSnapshot('basic');
+  });
+
+  it('should handle toast variations', async () => {
+    const toasts = [
+      {
+        alertType: undefined,
+        header: undefined,
+        message: 'Lorem ipsum',
+        removed: undefined,
+        paused: undefined
+      },
+      {
+        alertType: AlertVariant.success,
+        header: 'Dolor sit',
+        message: undefined,
+        removed: undefined,
+        paused: undefined
+      },
+      {
+        alertType: AlertVariant.danger,
+        header: 'Dolor sit',
+        message: 'Lorem ipsum',
+        removed: undefined,
+        paused: undefined
+      },
+      {
+        alertType: AlertVariant.info,
+        header: 'REMOVED, Dolor sit',
+        message: undefined,
+        removed: true,
+        paused: undefined
+      },
+      {
+        alertType: AlertVariant.info,
+        header: 'PAUSED, Dolor sit',
+        message: undefined,
+        removed: undefined,
+        paused: true
+      }
+    ];
+    const props = {
+      useSelector: () => toasts
+    };
+
+    const component = await shallowHookComponent(<ToastNotificationsList {...props} />);
     expect(component).toMatchSnapshot('variations');
+  });
+
+  it('should handle removing toasts through redux state with a hook', async () => {
+    const options = {};
+    const { result: onDismiss } = await shallowHook(() => useOnDismiss(options));
+
+    onDismiss({ mockToast: 'lorem ipsum' });
+    expect(mockDispatch.mock.calls).toMatchSnapshot('dispatch removing a toast, hook');
+  });
+
+  it('should handle pausing toasts through redux state with a hook', async () => {
+    const options = {};
+    const { result: onHover } = await shallowHook(() => useOnHover(options));
+
+    onHover({ mockToast: 'lorem ipsum' });
+    expect(mockDispatch.mock.calls).toMatchSnapshot('dispatch pausing a toast, hook');
+  });
+
+  it('should handle resuming toasts through redux state with a hook', async () => {
+    const options = {};
+    const { result: onLeave } = await shallowHook(() => useOnLeave(options));
+
+    onLeave({ mockToast: 'lorem ipsum' });
+    expect(mockDispatch.mock.calls).toMatchSnapshot('dispatch resuming a toast, hook');
   });
 });
