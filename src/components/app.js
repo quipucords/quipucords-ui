eef93d90604c95b6452e8619638a9e7ba682d426
CD Cabrera <ccabrera@redhat.com>
2022-10-10 11:15:53 -0400

    perf(app): app base to hooks (#180)

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connectRouter, reduxActions } from '../redux';
-import { helpers } from '../common';
-import I18n from './i18n/i18n';
+import { useMount } from 'react-use';
+import { reduxActions, storeHooks } from '../redux';
+import { I18n } from './i18n/i18n';
 import Authentication from './authentication/authentication';
 import PageLayout from './pageLayout/pageLayout';
 import { Router } from './router/router';
@@ -14,53 +14,61 @@ import CreateCredentialDialog from './createCredentialDialog/createCredentialDia
 import CreateScanDialog from './createScanDialog/createScanDialog';
 import MergeReportsDialog from './mergeReportsDialog/mergeReportsDialog';
 
-class App extends React.Component {
-  componentDidMount() {
-    const { getLocale } = this.props;
+/**
+ * Organize components into an app
+ *
+ * @param {object} props
+ * @param {Function} props.getLocale
+ * @param {Function} props.useDispatch
+ * @param {Function} props.useSelector
+ * @returns {React.ReactNode}
+ */
+const App = ({ getLocale, useDispatch: useAliasDispatch, useSelector: useAliasSelector }) => {
+  const dispatch = useAliasDispatch();
+  const locale = useAliasSelector(({ user }) => user?.session?.locale?.value, null);
 
-    getLocale();
-  }
+  useMount(() => {
+    dispatch(getLocale());
+  });
 
-  render() {
-    const { locale } = this.props;
+  return (
+    <I18n locale={locale}>
+      <Authentication>
+        <PageLayout>
+          <Router />
+          <ToastNotificationsList />
+          <ConfirmationModal />
+          <AboutModal />
+          <AddSourceWizard />
+          <CreateCredentialDialog />
+          <CreateScanDialog />
+          <MergeReportsDialog />
+        </PageLayout>
+      </Authentication>
+    </I18n>
+  );
+};
 
-    return (
-      <I18n locale={(locale && locale.value) || null}>
-        <Authentication>
-          <PageLayout>
-            <Router />
-            <ToastNotificationsList />
-            <ConfirmationModal />
-            <AboutModal />
-            <AddSourceWizard />
-            <CreateCredentialDialog />
-            <CreateScanDialog />
-            <MergeReportsDialog />
-          </PageLayout>
-        </Authentication>
-      </I18n>
-    );
-  }
-}
-
+/**
+ * Prop types
+ *
+ * @type {{useSelector: Function, useDispatch: Function, getLocale: Function}}
+ */
 App.propTypes = {
   getLocale: PropTypes.func,
-  locale: PropTypes.shape({
-    value: PropTypes.string
-  })
+  useDispatch: PropTypes.func,
+  useSelector: PropTypes.func
 };
 
+/**
+ * Prop types
+ *
+ * @type {{useSelector: Function, useDispatch: Function, getLocale: Function}}
+ */
 App.defaultProps = {
-  getLocale: helpers.noop,
-  locale: {}
+  getLocale: reduxActions.user.getLocale,
+  useDispatch: storeHooks.reactRedux.useDispatch,
+  useSelector: storeHooks.reactRedux.useSelector
 };
 
-const mapDispatchToProps = dispatch => ({
-  getLocale: () => dispatch(reduxActions.user.getLocale())
-});
-
-const mapStateToProps = state => ({ locale: state.user.session.locale });
-
-const ConnectedApp = connectRouter(mapStateToProps, mapDispatchToProps)(App);
-
-export { ConnectedApp as default, ConnectedApp, App };
+export { App as default, App };
