a2d98fd56ed88efc829b1c1beee5ec7ecfff154b
CD Cabrera <cabrera.code@gmail.com>
2024-09-05 17:00:06 -0400

    test: ds-742 remove typings, constants from coverage
    Relates to JIRA: DISCOVERY-742

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/.*/**', '!src/**/**/index.{ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/.*/**', '!src/**/**/index.{ts,tsx}', '!src/types/*.ts', '!src/constants/*.ts'],
   coverageThreshold: {
     global: {
       branches: 60,
