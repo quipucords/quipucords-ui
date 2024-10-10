bb5a9a2eb82f6021f040d07a2e8fbb1d008c695d
Mirek Długosz <mzalewsk@redhat.com>
2024-10-10 17:32:40 +0200

    fix(sources): ds-503 accept wider range of hosts values
    Allow user to delimit hosts (hostnames, IP addresses or IP ranges) by
    space or newline, in addition to comma. This brings new UI in line with
    old UI, which allowed wider range of user values and transformed them
    automatically before submitting to backend.
    
    Relates to JIRA: DISCOVERY-503

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -99,6 +99,19 @@ const getTimeDisplayHowLongAgo = (timestamp: MomentInput, { devMode = DEV_MODE }
 const formatDate = (date: Date) => moment.utc(date).format('DD MMMM Y, h:mm A z');
 
 /**
+ * Normalizes hosts textarea content into array that can be submitted to backend.
+ *
+ * @param {string} data - host textarea content.
+ * @returns Array of host values which will be submitted to backend.
+ */
+const normalizeHosts = (data?: string) =>
+  data
+    ?.trim()
+    ?.replaceAll(/\\n|\\r|\s/g, ',')
+    ?.split(',')
+    ?.filter(Boolean);
+
+/**
  * Normalizes a total count to a non-negative number less than a given modulus,
  * optionally based on development mode.
  *
@@ -222,6 +235,7 @@ const helpers = {
   getTimeDisplayHowLongAgo,
   getTitleImg,
   formatDate,
+  normalizeHosts,
   normalizeTotal,
   DEV_MODE,
   PROD_MODE,
