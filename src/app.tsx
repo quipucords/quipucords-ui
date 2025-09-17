77db7cda54bc87e5c94ed04c9e5ef92641a94c38
Mirek Długosz <mzalewsk@redhat.com>
2025-09-17 17:59:47 +0200

    view(overview): Prepare skeleton page
    The page is prepared according to the mockup, except:
    
    - the middle row should be a process description, to be filled later
    - it remains to be determined if bottom left will have text or an image
    - there are no answers to questions yet
    
    Relates to JIRA: DISCOVERY-1098

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,7 @@
  */
 import React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
+import '@patternfly/patternfly/utilities/_index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { I18n } from './components/i18n/i18n';
 import { Login } from './components/login/login';
